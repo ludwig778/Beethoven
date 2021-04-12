18d2b6580e55c184eb09770b0725c112475c69dd
Laurent Arthur <laurent.arthur75@gmail.com>
2021-04-12 22:26:53 +0200

    Fix sequencer scale missing coverage

--- tests/sequencer/test_scale.py
+++ tests/sequencer/test_scale.py
@@ -1,6 +1,7 @@
 from pytest import mark, raises
 
 from beethoven.sequencer.note import Note
+from beethoven.sequencer.chord import Chord
 from beethoven.sequencer.scale import Scale
 
 
@@ -10,10 +11,14 @@ from beethoven.sequencer.scale import Scale
     ("D4", "phrygian #6", "D4,Eb4,F4,G4,A4,B4,C5"),
     ("F4", "whole tone",  "F4,G4,A4,B4,Db5,Eb5")
 ])
-def test_scale(root_note, scale_name, result_notes):
+def test_scale_instanciation(root_note, scale_name, result_notes):
     scale = Scale(root_note, scale_name)
 
     assert scale.notes == Note.to_list(result_notes)
+
+
+def test_scale_repr():
+    assert repr(Scale("A4", "major")) == "<Scale A4 ionian>"
 
 
 @mark.parametrize("scale_name", ["foo", "bar"])
@@ -35,3 +40,30 @@ def test_scale_notation_on_initialization(name, alt_name):
 
     assert notation1 == notation2
     assert str(notation1) == str(notation2)
+
+
+@mark.parametrize("start_degree_name,chord_basic,chord_seventh", [
+    (1, Chord("B4",  "min"), Chord("B4",  "min7")),
+    (2, Chord("C#5", "min"), Chord("C#5", "min7")),
+    (3, Chord("D5",  "maj"), Chord("D5",  "maj7")),
+    (4, Chord("E5",  "maj"), Chord("E5",  "7")),
+    (6, Chord("G#5", "dim"), Chord("G#5", "min7b5")),
+])
+def test_scale_get_chords(start_degree_name, chord_basic, chord_seventh):
+    scale = Scale("A4", "major")
+
+    assert scale.get_chord(start_degree_name, "1,3,5") == chord_basic
+    assert scale.get_chord(start_degree_name, "1,3,5,7") == chord_seventh
+
+
+@mark.parametrize("start_degree_name,alteration,chord_basic,chord_seventh", [
+    (1, 0,  Chord("B4",   "min"), Chord("B4",   "min7")),
+    (1, 1,  Chord("B#4",  "min"), Chord("B#4",  "min7")),
+    (1, 2,  Chord("B##4", "min"), Chord("B##4", "min7")),
+    (1, -2, Chord("Bbb4", "min"), Chord("Bbb4", "min7")),
+])
+def test_scale_get_chord_with_alteration(start_degree_name, alteration, chord_basic, chord_seventh):
+    scale = Scale("A4", "major")
+
+    assert scale.get_chord(start_degree_name, "1,3,5", alteration=alteration) == chord_basic
+    assert scale.get_chord(start_degree_name, "1,3,5,7", alteration=alteration) == chord_seventh

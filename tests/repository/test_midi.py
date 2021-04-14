5cd314b6624d85116b952894ff2effba4ea4d5d2
Laurent Arthur <laurent.arthur75@gmail.com>
2021-04-14 02:08:05 +0200

    Update midi repository tests with some monkeypatching on midi messages

--- tests/repository/test_midi.py
+++ tests/repository/test_midi.py
@@ -1,8 +1,10 @@
+from functools import partial
+
 from beethoven.repository.midi import midi
 from beethoven.sequencer.note import Note
 
 
-def test_midi_play():
+def test_midi_play(monkeypatch):
     timeline = {
         0.0: [
             {
@@ -29,7 +31,27 @@ def test_midi_play():
     }
     length = 0.04
 
-    midi.play(timeline, length)
-    midi.play(timeline, length, repeat=2)
+    def get_messages(msg, messages=None):
+        messages.append({
+            "type": msg.type,
+            "channel": msg.channel,
+            "note": msg.note,
+            "velocity": msg.velocity,
+            "time": msg.time
+        })
 
-    assert midi
+    messages = []
+    monkeypatch.setattr(midi, "_send_msg", partial(get_messages, messages=messages))
+
+    midi.play(timeline, length)
+
+    assert messages == [
+        {'type': 'note_on',  'channel': 0, 'note': 69, 'velocity': 127, 'time': 0},
+        {'type': 'note_on',  'channel': 0, 'note': 73, 'velocity': 127, 'time': 0},
+        {'type': 'note_on',  'channel': 1, 'note': 62, 'velocity': 63,  'time': 0},
+        {'type': 'note_off', 'channel': 1, 'note': 62, 'velocity': 127, 'time': 0.020833333333333332},
+        {'type': 'note_on',  'channel': 1, 'note': 62, 'velocity': 63,  'time': 0},
+        {'type': 'note_off', 'channel': 0, 'note': 69, 'velocity': 127, 'time': 0.020833333333333332},
+        {'type': 'note_off', 'channel': 0, 'note': 73, 'velocity': 127, 'time': 0},
+        {'type': 'note_off', 'channel': 1, 'note': 62, 'velocity': 127, 'time': 0}
+    ]

e6d02d12fc020cd6ef743149c0b56bfee71bcf4b
Laurent Arthur <laurent.arthur75@gmail.com>
2021-07-18 14:07:59 +0200

    Simplify parsing validation functions

--- beethoven/prompt/validator.py
+++ beethoven/prompt/validator.py
@@ -1,5 +1,3 @@
-from pyparsing import ParseFatalException
-
 from beethoven.sequencer.tempo import Tempo
 from beethoven.sequencer.time_signature import TimeSignature
 from beethoven.theory.chord import Chord
@@ -8,23 +6,27 @@ from beethoven.theory.note import Note
 from beethoven.theory.scale import Scale
 
 
+class ValidationError(ValueError):
+    pass
+
+
 def handle_parser_exception(func):
-    def wrapper(string, loc, expr):
+    def wrapper(*args):
         try:
-            func(string, loc, expr)
+            func(*args)
         except Exception as exc:
-            raise ParseFatalException(string, loc, str(exc))
+            raise ValidationError(f"Parsing validation error: {str(exc)}")
     return wrapper
 
 
 @handle_parser_exception
-def validate_scale(string, loc, expr):
-    Scale("A", expr[0])
+def validate_scale(name):
+    Scale("A", name)
 
 
 @handle_parser_exception
-def validate_note(string, loc, expr):
-    Note(expr[0])
+def validate_note(name):
+    Note(name)
 
 
 @handle_parser_exception
@@ -36,19 +38,19 @@ def validate_note_or_degree(string, loc, expr):
 
 
 @handle_parser_exception
-def validate_tempo(string, loc, expr):
-    Tempo(expr[0])
+def validate_tempo(num):
+    Tempo(num)
 
 
 @handle_parser_exception
-def validate_time_signature(string, loc, expr):
-    TimeSignature(*expr)
+def validate_time_signature(beat, per_bar):
+    TimeSignature(beat, per_bar)
 
 
 @handle_parser_exception
-def validate_chord_item(string, loc, expr):
+def validate_chord_item(item):
     if not (
-        Chord.get_from_fullname(expr[0]) or
-        Harmony(Scale("A", "major")).get(expr[0])
+        Chord.get_from_fullname(item) or
+        Harmony(Scale("A", "major")).get(item)
     ):
         raise Exception("Couldn't parse chord item")

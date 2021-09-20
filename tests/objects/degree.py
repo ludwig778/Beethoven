3c3ab5bb453e03555b66b00cde8b75e5b26f189b
Laurent Arthur <laurent.arthur75@gmail.com>
2021-09-20 21:54:12 +0200

    Add degree serialization test

--- tests/objects/degree.py
+++ tests/objects/degree.py
@@ -14,3 +14,16 @@ from beethoven.objects import Degree
 )
 def test_degree_parsing(string, degree):
     assert Degree.parse(string) == degree
+
+
+@mark.parametrize(
+    "degree,string",
+    [
+        (Degree(name="I", alteration=0), "I"),
+        (Degree(name="ii", alteration=-1), "bii"),
+        (Degree(name="III", alteration=0), "III"),
+        (Degree(name="v", alteration=1), "#v"),
+    ],
+)
+def test_degree_serialization(degree, string):
+    assert degree.serialize() == string

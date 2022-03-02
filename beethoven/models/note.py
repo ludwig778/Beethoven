22a33692400809d191c0837c130de877addc44a6
Arthur Laurent <laurent.arthur75@gmail.com>
2022-03-02 01:50:24 +0100

    Add Note model comparison methods

--- beethoven/models/note.py
+++ beethoven/models/note.py
@@ -32,3 +32,37 @@ class Note(BaseModel):
             return octave
 
         raise ValueError(f"Invalid octave: {octave}, must be between 0 and 10")
+
+    @property
+    def midi_index(self) -> int:
+        return (
+            note_index.get_semitones(self.name)
+            + self.alteration
+            + (self.octave or 0) * 12
+        )
+
+    def check_octave_states(self, other: Note) -> None:
+        """Check that both notes have or have not octaves"""
+
+        # Apply a XOR function checking on None values
+        if [self.octave is None, other.octave is None].count(False) == 1:
+            raise Exception(
+                "Octaves must be present or absent in order to compare Notes"
+            )
+
+    def __eq__(self, other: Note) -> bool:
+        """Check notes pitch equality, since we check on the midi_index property"""
+
+        self.check_octave_states(other)
+
+        return self.midi_index == other.midi_index
+
+    def __gt__(self, other: Note) -> bool:
+        self.check_octave_states(other)
+
+        return self.midi_index > other.midi_index
+
+    def __ge__(self, other: Note) -> bool:
+        self.check_octave_states(other)
+
+        return self.midi_index >= other.midi_index
--- tests/models/test_note.py
+++ tests/models/test_note.py
@@ -33,3 +33,32 @@ def test_note_model_raise_invalid_octave(octave):
         ValueError, match=f"Invalid octave: {octave}, must be between 0 and 10"
     ):
         Note(name="C", octave=octave)
+
+
+def test_note_model_greater_equality_methods():
+    assert Note(name="C") == Note(name="C")
+    assert Note(name="C", alteration=1) == Note(name="C", alteration=1)
+    assert Note(name="C", alteration=1, octave=4) == Note(
+        name="C", alteration=1, octave=4
+    )
+
+    assert Note(name="C") != Note(name="C", alteration=1)
+
+    # Check enharmonic notes
+    assert Note(name="C") == Note(name="D", alteration=-2)
+
+
+def test_note_model_greater_equality_methods_raise_octave_state_discrepancy():
+    with raises(
+        Exception, match="Octaves must be present or absent in order to compare Notes"
+    ):
+        Note(name="C") == Note(name="C", octave=1)
+
+
+def test_note_model_greater_than_method():
+    assert Note(name="C") < Note(name="B")
+    assert Note(name="C", octave=1) > Note(name="B", octave=0)
+    assert Note(name="C", octave=1) < Note(name="B", octave=1)
+
+    assert Note(name="C") <= Note(name="D")
+    assert Note(name="C") <= Note(name="B")

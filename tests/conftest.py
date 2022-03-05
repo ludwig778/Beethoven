7a2fda5c65876e59d276da0c5e2127ba9bc38ba8
Arthur Laurent <laurent.arthur75@gmail.com>
2022-03-05 01:23:35 +0100

    Fix platform dependent midi tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,28 @@
 from pathlib import Path
 
 from hartware_lib.adapters.directory import DirectoryAdapter
+from mido.backends.rtmidi import Output
 from pytest import fixture
 
 from beethoven.adapters import get_adapters
+
+
+@fixture(scope="function", autouse=True)
+def mock_open_midi_output(monkeypatch):
+    def void(self, *args, **kwargs):
+        pass
+
+    class MockedOutput(Output):
+        def __init__(self, name, **kwargs):
+            self.name = name
+
+        send = void
+        _send = void
+        panic = void
+        close = void
+
+    monkeypatch.setattr("beethoven.adapters.midi.Output", MockedOutput)
+    monkeypatch.setattr("beethoven.adapters.midi.open_output", MockedOutput)
 
 
 @fixture(scope="function", autouse=True)

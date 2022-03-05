3199eb6e443d921ae78e0fccb4d2ae824d491f14
Arthur Laurent <laurent.arthur75@gmail.com>
2022-03-05 16:10:18 +0100

    Fix time signature controller construct method name

--- beethoven/controllers/time_signature.py
+++ beethoven/controllers/time_signature.py
@@ -7,10 +7,10 @@ class TimeSignatureController:
     def parse(cls, string: str) -> TimeSignature:
         parsed = parse_model_string("time_signature", string)
 
-        return cls.constuct(parsed)
+        return cls.construct(parsed)
 
     @classmethod
-    def constuct(cls, parsed: dict) -> TimeSignature:
+    def construct(cls, parsed: dict) -> TimeSignature:
         return TimeSignature(
             beats_per_bar=parsed["beats_per_bar"], beat_unit=parsed["beat_unit"]
         )

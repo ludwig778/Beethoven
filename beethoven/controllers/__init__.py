34ac356ed88c76e1b339b01da242608b8cb94f84
Arthur Laurent <laurent.arthur75@gmail.com>
2022-03-06 16:17:23 +0100

    Remove unused controllers files

--- beethoven/controllers/__init__.py
+++ beethoven/controllers/__init__.py
@@ -5,11 +5,9 @@ from beethoven.controllers.chord import ChordController
 from beethoven.controllers.degree import DegreeController
 from beethoven.controllers.duration import DurationController
 from beethoven.controllers.grid import GridController
-from beethoven.controllers.grid_part import GridPartController
 from beethoven.controllers.interval import IntervalController
 from beethoven.controllers.note import NoteController
 from beethoven.controllers.scale import ScaleController
-from beethoven.controllers.time_section import TimeSectionController
 from beethoven.controllers.time_signature import TimeSignatureController
 
 __all__: Any = (
@@ -18,10 +16,8 @@ __all__: Any = (
     DegreeController,
     DurationController,
     GridController,
-    GridPartController,
     IntervalController,
     NoteController,
     ScaleController,
-    TimeSectionController,
     TimeSignatureController,
 )
--- beethoven/controllers/time_section.py
+++
@@ -1,2 +0,0 @@
-class TimeSectionController:
-    pass

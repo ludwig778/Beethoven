78a96863f526c9e62bad03d0f6a5a5f2eea24e59
Arthur Laurent <laurent.arthur75@gmail.com>
2022-10-10 00:49:23 +0200

    Add integration tests, mainly UI dialogs

---
+++ tests/integration/dialogs.py
@@ -0,0 +1,95 @@
+from pytest import fixture
+from pathlib import Path
+from functools import partial
+from beethoven.adapters.midi import MidiAdapter
+from beethoven.ui.components.control import PlayerControlWidget, PlayingType
+from beethoven.ui.components.scale_picker import ScalePicker
+from beethoven.ui.dialogs import TuningDialog
+from PySide6.QtWidgets import QApplication
+from beethoven.ui.dialogs.chord_picker import ChordPickerDialog
+from beethoven.ui.dialogs.midi import MidiDialog
+from beethoven.ui.dialogs.player import PlayerDialog
+from beethoven.ui.main_window import ComposeWidget, MainWindow
+import beethoven.instruments  # noqa # pylint: disable=unused-import
+
+from beethoven.ui.managers import AppManager
+from beethoven.ui.models import HarmonyItems
+from beethoven.ui.utils import get_default_harmony_item
+
+
+@fixture
+def application(monkeypatch):
+    def lmao(*args, **kwargs):
+        pass
+        # print("LMAO", args, kwargs)
+
+    monkeypatch.setattr(MidiAdapter, "send_message", lmao)
+
+    yield QApplication([])
+
+
+def test_dialogs(application):
+    print("\n\n")
+
+    setting_path = Path(".", "config.ui.json")
+
+    default_harmony_item = get_default_harmony_item()
+
+    manager = AppManager(setting_path=setting_path)
+
+    # harmony_items = HarmonyItems.from_list([
+    #     {
+    #         "scale": "C4_ionian",
+    #         "chord_items": [
+    #             # {"root": "III", "name": "maj7"},
+    #             {"root": "A", "name": ""},
+    #             {"root": "D", "name": ""},
+    #             # {"root": "E", "name": ""},
+    #         ]
+    #     },
+    # ])
+
+    widget_partials = {
+        "tuning": partial(TuningDialog, tuning_settings=manager.settings.tuning),
+        "chord_picker": partial(
+            ChordPickerDialog,
+            manager=manager,
+            current_chord_item=default_harmony_item.chord_items[0],
+            current_scale=default_harmony_item.scale
+        ),
+        "midi_dialog": partial(
+            MidiDialog,
+            manager=manager,
+        ),
+        "player_dialog": partial(
+            PlayerDialog,
+            manager=manager,
+        ),
+        "scale_picker": partial(
+            ScalePicker,
+            current_scale=default_harmony_item.scale,
+        ),
+        "player_control": partial(
+            PlayerControlWidget,
+            harmony_items=default_harmony_item,
+            playing_type=PlayingType.none
+        ),
+        "compose_widget": partial(
+            ComposeWidget,
+            manager=manager,
+            harmony_items=HarmonyItems(items=[default_harmony_item]),
+        ),
+        "main_window": partial(
+            MainWindow,
+            manager=manager,
+        )
+    }
+    for name, widget_partial in widget_partials.items():
+        if name not in ("main_window"):
+            continue
+
+        widget = widget_partial()
+        widget.setWindowTitle("LMAO")
+        widget.show()
+
+        application.exec()

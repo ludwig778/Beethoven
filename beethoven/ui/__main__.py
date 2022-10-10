f21fad041c3522f46555a8fe581ac116744a7632
Arthur Laurent <laurent.arthur75@gmail.com>
2022-10-10 23:59:06 +0200

    Use existing component on ui app

--- beethoven/ui/__main__.py
+++ beethoven/ui/__main__.py
@@ -1,135 +1,21 @@
 import atexit
-from enum import Enum, auto
 from pathlib import Path
 from logging import getLogger
 from beethoven import controllers
-from beethoven.helpers.note import remove_note_octave
 from beethoven.models.grid import Grid
 from beethoven.models.grid_part import GridPart
-from beethoven.models.scale import Scale
-from beethoven.ui.components.buttons import Button, PushPullButton
 from beethoven.ui.components.chord_picker import ChordPicker
-from beethoven.ui.components.combobox import NoteComboBox, ScaleComboBox
+from beethoven.ui.components.control import PlayerControlWidget, PlayingType
 from beethoven.ui.components.harmony_grid import HarmonyGrid
+from beethoven.ui.components.scale_picker import ScalePicker
 from beethoven.ui.layouts import horizontal_layout, vertical_layout
 from beethoven.ui.managers import AppManager
 from beethoven.ui.models import HarmonyItems
 from beethoven.ui.stylesheet import get_stylesheet
 from PySide6.QtGui import QKeySequence, QShortcut
-from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QSpinBox, QWidget, QApplication
-
-from beethoven.ui.utils import block_signal
+from PySide6.QtWidgets import QWidget, QApplication
 
 logger = getLogger("ui.main")
-
-
-class ScaleSelector(QWidget):
-    scale_changed = Signal(Scale)
-
-    def __init__(self, *args, current_scale: Scale, **kwargs):
-        super(ScaleSelector, self).__init__(*args, **kwargs)
-
-        self.root_combobox = NoteComboBox(
-            selected_note=remove_note_octave(current_scale.tonic)
-        )
-        self.name_combobox = ScaleComboBox(selected_scale_name=current_scale.name)
-        self.octave_spinbox = QSpinBox()
-        self.octave_spinbox.setMinimum(0)
-        self.octave_spinbox.setMaximum(10)
-        self.octave_spinbox.setValue(current_scale.tonic.octave or 4)
-
-        self.root_combobox.note_changed.connect(self.update_scale)
-        self.name_combobox.scale_name_changed.connect(self.update_scale)
-        self.octave_spinbox.valueChanged.connect(self.update_scale)
-
-        self.setLayout(
-            horizontal_layout([
-                self.root_combobox,
-                self.name_combobox,
-                self.octave_spinbox,
-            ])
-        )
-
-    def set_scale(self, scale):
-        self.root_combobox.set_note(scale.tonic)
-        self.name_combobox.set_scale_name(scale.name)
-        self.octave_spinbox.setValue(scale.tonic.octave or 4)
-
-    def get_scale(self):
-        tonic = self.root_combobox.get_note()
-        scale_name = self.name_combobox.get_scale_name()
-        octave = self.octave_spinbox.value()
-
-        return controllers.scale.parse(f"{tonic}{octave}_{scale_name}")
-
-    def update_scale(self, *args):
-        self.scale_changed.emit(self.get_scale())
-
-
-class PlayingType(Enum):
-    NONE = auto()
-    STEP = auto()
-
-
-class PlayerWidget(QWidget):
-    play_grid = Signal()
-    play_grid_step = Signal()
-    stop_grid = Signal()
-
-    def __init__(
-        self, *args, harmony_items: HarmonyItems, playing_type: PlayingType, **kwargs
-    ):
-        super(PlayerWidget, self).__init__(*args, **kwargs)
-
-        self.harmony_items = harmony_items
-
-        self.step_player_button = PushPullButton(
-            pressed="Step", released="Step", state=playing_type == PlayingType.STEP
-        )
-        self.play_button = PushPullButton(pressed="Play", released="Play", state=False)
-        self.stop_button = Button("Stop")
-
-        self.step_player_button.toggled.connect(self.step_play)
-        self.play_button.toggled.connect(self.play)
-        self.stop_button.clicked.connect(self.clear)
-
-        self.setLayout(
-            horizontal_layout([
-                self.step_player_button,
-                self.play_button,
-                self.stop_button,
-            ])
-        )
-
-    def step_play(self, state):
-        print("STEP PLAY", state)
-        if self.play_button.is_pressed:
-            with block_signal([self.play_button]):
-                self.play_button.release()
-
-            self.stop_grid.emit()
-
-        if state:
-            self.play_grid_step.emit()
-
-    def play(self, state):
-        print("PLAY", state)
-        if self.step_player_button.is_pressed:
-            with block_signal([self.step_player_button]):
-                self.step_player_button.release()
-
-            self.stop_grid.emit()
-
-        if state:
-            self.play_grid.emit()
-
-    def clear(self):
-        with block_signal([self.step_player_button, self.play_button]):
-            self.step_player_button.release()
-            self.play_button.release()
-
-        self.stop_grid.emit()
 
 
 class App(QWidget):
@@ -148,8 +34,8 @@ class App(QWidget):
 
         harmony_item, chord_item = self.harmony_grid.get_current_item()
 
-        self.player_widget = PlayerWidget(
-            harmony_items=harmony_items, playing_type=PlayingType.STEP
+        self.player_widget = PlayerControlWidget(
+            harmony_items=harmony_items, playing_type=PlayingType.step
         )
 
         self.chord_picker = ChordPicker(
@@ -157,7 +43,7 @@ class App(QWidget):
             current_chord_item=chord_item,
             current_scale=harmony_item.scale,
         )
-        self.scale_selector = ScaleSelector(current_scale=harmony_item.scale)
+        self.scale_selector = ScalePicker(current_scale=harmony_item.scale)
 
         self.chord_picker.chord_item_changed.connect(self.selected_chord_item_changed)
         self.scale_selector.scale_changed.connect(self.selected_scale_changed)

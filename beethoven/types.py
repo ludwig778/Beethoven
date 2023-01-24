fe4afe7664d556429dbca31592062cf08a405eb3
Arthur Laurent <laurent.arthur75@gmail.com>
2023-01-24 18:06:55 +0100

    Set NotesContainer protocol for good

--- beethoven/types.py
+++ beethoven/types.py
@@ -1,15 +1,12 @@
 from typing import List, Protocol
 
-from beethoven.models import GridPart
-
-
-class NotesContainer(Protocol):
-    @property
-    def notes(self):
-        ...
-
-    def __hash__(self):
-        return hash(":".join([str(hash(note)) for note in self.notes]))
+from beethoven.models import GridPart, Note
 
 
 GridParts = List[GridPart]
+
+
+class NotesContainer(Protocol):
+    notes: List[Note]
+
+    def __hash__(self): ...
